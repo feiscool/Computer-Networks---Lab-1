09b14c33e36faea240989b167a19c930d61c575b
Savannah Forood <sforood@gmail.com>
2016-09-19 19:37:06 -0500

    Server data (to send to client) updated.
    To do: The result print out (after sending back to client) doesn’t make
    sense.

--- UDPserver.py
+++ UDPserver.py
@@ -4,6 +4,7 @@ import struct
 import binascii
 
 errorCode = 0
+result = 0
 reply = bytearray()
 
 # Create a TCP/IP socket
@@ -20,21 +21,34 @@ while True:
     dataReceived, address = sock.recvfrom(1024)
     
     print >>sys.stderr, 'received %s bytes from %s' % (len(dataReceived), address)
-    print >>sys.stderr, dataReceived
 
     dataCharArray = list(binascii.hexlify(dataReceived))
 
     #parse data
-    TMLReceived = int(dataCharArray[0]) + int(dataCharArray[1])
-    reqID = int(dataCharArray[2]) + int(dataCharArray[3])
-    opcode = int(dataCharArray[4]) + int(dataCharArray[5])  
-    numOperands = int(dataCharArray[6]) + int(dataCharArray[7]) 
-    operand1 = int(dataCharArray[8]) + int(dataCharArray[9]) + int(dataCharArray[10]) + int(dataCharArray[11])
-    operand2 = int(dataCharArray[12]) + int(dataCharArray[13]) + int(dataCharArray[14]) + int(dataCharArray[15])
+    TMLReceivedIn = str(dataCharArray[0]) + str(dataCharArray[1])
+    reqIDIn = str(dataCharArray[2]) + str(dataCharArray[3])
+    opcodeIn = str(dataCharArray[4]) + str(dataCharArray[5])  
+    numOperands = str(dataCharArray[6]) + str(dataCharArray[7]) 
+    if (str(dataCharArray[10]) + str(dataCharArray[11]) == '00'):
+    	op1In = str(dataCharArray[8]) + str(dataCharArray[9])
+    else:	
+    	op1In = str(dataCharArray[8]) + str(dataCharArray[9]) + str(dataCharArray[10]) + str(dataCharArray[11])
+    if (str(dataCharArray[14]) + str(dataCharArray[15]) == '00'):
+    	op2In = str(dataCharArray[12]) + str(dataCharArray[13])
+    else:
+    	op2In = str(dataCharArray[12]) + str(dataCharArray[13]) + str(dataCharArray[14]) + str(dataCharArray[15])
+
+    #convert to int
+    TMLReceived = int(TMLReceivedIn)
+    reqID = int(reqIDIn)
+    opcode = int(opcodeIn)
+  
+    operand1 = int(op1In, 16)
+    operand2 = int(op2In, 16)
 
    #do calculation here
     if (opcode == 0):
-        result = int(operand1) + int(operand2)
+        result = operand1 + operand2
     if (opcode == 1): 
         result = int(operand1) - int(operand2)
     if (opcode == 2):  
@@ -46,8 +60,6 @@ while True:
     if (opcode == 5): 
         result = int(operand1) << int(operand2)
 
-    print('result: %d', result)
-    
     #construct structResponse
     reply.append(TMLReceived)
     reply.append(reqID)
@@ -55,14 +67,20 @@ while True:
     
     resultLen = len(str(result))
     zerosNeeded = 4 - resultLen
+    resultArray = [int(x) for x in str(result)]
+    #place each byte from the result,
+	#into byte array, starting at index 4.
 
-    reply.append(result)
-
-    for x in range(0, zerosNeeded):
+    for x in range (0, zerosNeeded):
     	reply.append(0)
 
+    for j in range (0, len(resultArray)):
+        reply.append(resultArray[j])   
+
+    print("***contents of reply data**")
     for x in reply:
         print(x)
+    print("*****")
 
     if reply:
         sent = sock.sendto(reply, address)
